6c50fc76f557ae00ce603441cbe92a9d793b5987
Comamoca <comamoca.dev@gmail.com>
2023-04-27 23:56:50 +0900

    :sparkles: (skkleton.lua) swhitch to toggle keybind

--- config/nvim/lua/configs/skkeleton.lua
+++ config/nvim/lua/configs/skkeleton.lua
@@ -2,6 +2,5 @@ vim.call("skkeleton#config", { globalJisyo = "~/.skk/SKK-JISYO.L" })
 
 local kmap = vim.keymap.set
 
-kmap("i", "jk", "<Plug>(skkeleton-enable)")
-kmap("i", "C-j", "<Plug>(skkeleton-enable)")
-kmap("c", "jk", "<Plug>(skkeleton-enable")
+kmap("i", "<C-j>", "<Plug>(skkeleton-toggle)")
+kmap("c", "<C-j>", "<Plug>(skkeleton-toggle)")
