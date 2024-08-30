e77cda54f92445948c8c6f5a8d081c93214b16a6
Comamoca <comamoca.dev@gmail.com>
2024-08-30 09:19:51 +0900

    :sparkles: (.emacs.d/init.el) Add clipboard integration.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -109,3 +109,21 @@
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 
 (electric-pair-mode 1)
+
+
+;; Copy & Paste with wl-clipboard
+;; ref: https://gist.github.com/yorickvP/6132f237fbc289a45c808d8d75e0e1fb
+(setq wl-copy-process nil)
+(defun wl-copy (text)
+  (setq wl-copy-process (make-process :name "wl-copy"
+                                      :buffer nil
+                                      :command '("wl-copy" "-f" "-n")
+                                      :connection-type 'pipe))
+  (process-send-string wl-copy-process text)
+  (process-send-eof wl-copy-process))
+(defun wl-paste ()
+  (if (and wl-copy-process (process-live-p wl-copy-process))
+      nil ; should return nil if we're the current paste owner
+      (shell-command-to-string "wl-paste -n | tr -d \r")))
+
+(setq interprogram-paste-function 'wl-paste)
