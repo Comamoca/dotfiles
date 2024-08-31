4b36cf4f2803a75acd7575ca74b67d0b2b9cfafb
Comamoca <comamoca.dev@gmail.com>
2024-08-31 16:32:18 +0900

    :sparkles: (scripts/ime/ime.sh) Update launch kitty command.

--- bin/scripts/ime/ime.sh
+++ bin/scripts/ime/ime.sh
@@ -1,12 +1,21 @@
 #!/bin/bash -u
 
+nvim_bin="$HOME/.nix-profile/bin/nvim"
+# nvim_bin="nvim"
+
+# kitty_bin="$HOME/.nix-profile/bin/kitty"
+kitty_bin="nixGLIntel kitty"
+
 if [ $# -ne 1  ]; then
-	env NVIM_APPNAME=ime nvim /tmp/clip -c startinsert
+	env NVIM_APPNAME=ime $nvim_bin /tmp/clip -c startinsert
+	head -c -1 /tmp/clip | wl-copy
+	rm -f /tmp/clip
 	exit
 fi
 
 if [ "$1" = 'float' ]; then
-	kitty --class Floaterm env NVIM_APPNAME=ime nvim /tmp/clip -c startinsert 
+	$kitty_bin --class Floaterm env NVIM_APPNAME=ime $nvim_bin /tmp/clip -c startinsert
+	# kitty --class Floaterm env vim /tmp/clip -c startinsert
 
 	if [[ -e /tmp/clip ]]; then
 		head -c -1 /tmp/clip | wl-copy
