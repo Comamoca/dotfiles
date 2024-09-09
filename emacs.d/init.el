15b3e804626d470ccd6742a926088ce92705a6d5
Comamoca <comamoca.dev@gmail.com>
2024-09-09 13:07:05 +0900

    :sparkles: (emacs.d/init.el) Update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,19 +1,50 @@
-(require 'package)
+;; ========== leaf.el setup start  ==========     
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org" . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu" . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
 
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    ;;(leaf hydra :ensure t)
+    ;;(leaf el-get :ensure t)
+    ;;(leaf blackout :ensure t)
 
-(package-initialize)
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
 
-;; Load and setup packages
-(use-package lsp-mode)
-(use-package ivy)
-(use-package paredit)
-(use-package highlight-indent-guides)
+;; ========== leaf.el setup end  ==========
 
-(use-package company-go)
-(use-package company
+;; Now you can use leaf!
+
+;; (leaf leaf-tree :ensure t)
+;; (leaf leaf-convert :ensure t)
+;; (leaf transient-dwim
+;;   :ensure t
+;;   :bind (("M-=" . transient-dwim-dispatch)))
+
+(leaf kanagawa-theme :ensure t
+  :preface
+  :config
+  (load-theme 'kanagawa t))
+
+(leaf lsp-mode :ensure t)
+(leaf ivy :ensure t)
+(leaf paredit :ensure t)
+(leaf highlight-indent-guides :ensure t)
+
+(leaf company-go :ensure t)
+
+(leaf company
+  :ensure t
   :init
   (global-company-mode)
   (setq company-idle-delay 0.3)
@@ -21,94 +52,33 @@
   (setq company-transformers '(company-sort-by-occurrence))
   :bind
   (:map company-active-map
-	("C-n". company-select-next)
-	("C-p". company-select-previous)
-	("M-<". company-select-first)
-	("M->". company-select-last)))
+	("C-n". company-select-next)))
 
-;; Language modes
-(use-package go-mode
-      :hook
-      (go-mode . eglot-ensure))
-
-(use-package c++-mode
-      :hook
-      (c++-mode . eglot-ensure))
-
-(use-package sh-mode
-      :hook
-      (sh-mode . eglot-ensure))
-
-(use-package html-mode
-      :hook
-      (html-mode . eglot-ensure))
-
-(use-package cmake-mode
-      :hook
-      (cmake-mode . eglot-ensure))
-
-(use-package bitbake-mode
-      :hook
-      (bitbake-mode . eglot-ensure))
-
-;; eglot(LSP)
-(use-package eglot
+(leaf meow
   :ensure t
   :config
-  (add-to-list 'eglot-server-programs
-        '((go-mode) "gopls"))
-        ;; '((mode-name) "program-name")
-   
-  :bind (("M-t" . xref-find-definitions)
-         ("M-r" . xref-find-references)
-         ("C-t" . xref-go-back)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(highlight-indent-guides paredit company-box ivy lsp-ui company-go company leaf go-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- 
+  (defun meow-setup nil
+    )
 
-(leaf iceberg-theme
-  :ensure t
-  :config
-  (iceberg-theme-create-theme-file)
-  (load-theme 'solarized-iceberg-dark t))
+  (meow-setup)
+  (meow-global-mode 1)
+  )
 
-(defun lsp-mode-init ()
-  (lsp)
-  (global-set-key (kbd "M-*") 'xref-pop-marker-stack)
-  (global-set-key (kbd "M-.") 'xref-find-definitions)
-  (global-set-key (kbd "M-/") 'xref-find-references))
+;; Emacs evil-mode
+ (leaf evil
+   :ensure t
+   :init
+   (evil-mode 1)
+   (define-key evil-insert-state-map "jj" #'evil-normal-state))
 
-;; lsp-ui config
-(setq lsp-ui-doc-enable t)
-(setq lsp-ui-doc-header t)
-(setq lsp-ui-doc-include-signature t)
-(setq lsp-ui-doc-max-width 150)
-(setq lsp-ui-doc-max-height 30)
-(setq lsp-ui-peek-enable t)
-(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+;; Follow symlinks
+(setq vc-follow-symlinks t)
 
 
-(global-set-key (kbd "C-c <left>")  'windmove-left)
-(global-set-key (kbd "C-c <down>")  'windmove-down)
-(global-set-key (kbd "C-c <up>")    'windmove-up)
-(global-set-key (kbd "C-c <right>") 'windmove-right)
-
-(electric-pair-mode 1)
+(defun initel ()
+  "open init.el"
+  (interactive)
+  (find-file user-init-file))
 
 
 ;; Copy & Paste with wl-clipboard
