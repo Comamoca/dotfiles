aac150139e946faa499c785afcdb1b8442fdf4e2
Comamoca <comamoca.dev@gmail.com>
2024-09-09 18:21:16 +0900

    :tada: (config/emacs) Add new configration.

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -22,3 +22,63 @@
     (leaf-keywords-init)))
 ;; </leaf-install-code>
 
+;; ================================================
+
+(leaf kanagawa-theme :ensure t
+  :preface
+  :config
+  (load-theme 'kanagawa t))
+
+(leaf lsp-mode :ensure t)
+(leaf ivy :ensure t)
+(leaf paredit :ensure t)
+(leaf highlight-indent-guides :ensure t
+
+;; Emacs evil-mode
+ (leaf evil
+   :ensure t
+   :init
+   (evil-mode 1)
+   (define-key evil-insert-state-map "jj" #'evil-normal-state)))
+
+(setq vc-follow-symlinks t)
+
+;; initel function that behaves like `:e $MYVIMRC`
+(defun initel ()
+  "open init.el"
+  (interactive)
+  (find-file user-init-file))
+
+;; Copy & Paste with wl-clipboard
+;; ref: https://gist.github.com/yorickvP/6132f237fbc289a45c808d8d75e0e1fb
+(setq wl-copy-process nil)
+(defun wl-copy (text)
+  (setq wl-copy-process (make-process :name "wl-copy"
+                                      :buffer nil
+                                      :command '("wl-copy" "-f" "-n")
+                                      :connection-type 'pipe))
+  (process-send-string wl-copy-process text)
+  (process-send-eof wl-copy-process))
+(defun wl-paste ()
+  (if (and wl-copy-process (process-live-p wl-copy-process))
+      nil ; should return nil if we're the current paste owner
+      (shell-command-to-string "wl-paste -n | tr -d \r")))
+
+(setq interprogram-paste-function 'wl-paste)
+
+;; ================================================
+
+(leaf leaf
+  :config
+  (leaf leaf-convert :ensure t)
+  (leaf leaf-tree
+    :ensure t
+    :custom ((imenu-list-size . 30)
+             (imenu-list-position . 'left))))
+
+(leaf macrostep
+  :ensure t
+  :bind (("C-c e" . macrostep-expand)))
+
+
+(provide 'init)
