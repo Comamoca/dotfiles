efc73d2dc08fe926a023c83ab72e25cb503bb307
Comamoca <comamoca.dev@gmail.com>
2024-10-30 16:38:58 +0900

    :sparkles: (config/emacs) Move to emacs.d

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -131,7 +131,6 @@
   :init
   (global-corfu-mode))
 
-
 ;; ======================= My Configuration =======================
 
 (electric-pair-mode 1)
@@ -161,7 +160,7 @@
   (setq wl-copy-process (make-process :name "wl-copy"
                                       :buffer nil
                                       :command '("wl-copy" "-f" "-n")
-				      ;; :command '("wl-copy")
+              ;; :command '("wl-copy")
                                       :connection-type 'pipe))
   (process-send-string wl-copy-process text)
   (process-send-eof wl-copy-process))
@@ -169,10 +168,10 @@
 
 (defun wl-paste ()
   (if (and wl-copy-process (process-live-p wl-copy-process))
-      nil	  ; should return nil if we're the current paste owner
+      nil    ; should return nil if we're the current paste owner
     ;; (shell-command-to-string "wl-paste -n | tr -d \r")
-    (shell-command-to-string "wl-paste -n")
-    ))
+    (shell-command-to-string "wl-paste -n")))
+    
 
 (setq interprogram-cut-function 'wl-copy)
 (setq interprogram-paste-function 'wl-paste)
