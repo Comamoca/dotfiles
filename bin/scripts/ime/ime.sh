330a69d22600f9de5bc1073ba7eacb935668a7c2
Comamoca <comamoca.dev@gmail.com>
2024-11-03 20:36:09 +0900

    :sparkles: (ime.sh) Change use terminal to foot

--- bin/scripts/ime/ime.sh
+++ bin/scripts/ime/ime.sh
@@ -15,7 +15,8 @@ if [ $# -ne 1  ]; then
 fi
 
 if [ "$1" = 'float' ]; then
-	$kitty_bin --class Floaterm env NVIM_APPNAME=ime $nvim_bin /tmp/clip -c startinsert
+	# $kitty_bin --class Floaterm env NVIM_APPNAME=ime $nvim_bin /tmp/clip -c startinsert
+	foot -T Floaterm env NVIM_APPNAME=ime $nvim_bin /tmp/clip -c startinsert
 	# kitty --class Floaterm env vim /tmp/clip -c startinsert
 
 	if [[ -e /tmp/clip ]]; then
