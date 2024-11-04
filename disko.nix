828610da78c5bdbacb3ae9d0ab0ae07774875753
Comamoca <comamoca.dev@gmail.com>
2024-11-05 08:16:14 +0900

    :sparkles: (disko.nix) Update

--- disko.nix
+++ disko.nix
@@ -5,6 +5,8 @@
         type = "disk";
         content = {
           type = "gpt";
+	  device = "/dev/disk/by-uuid/c6bcd4ba-1fa7-4d52-aa51-c692063690d7";
+	  # device = "/dev/disk/by-id/root";
           partitions = {
             root = {
               size = "100%";
@@ -21,6 +23,8 @@
             ESP = {
               size = "1G";
               type = "EF00";
+	      device = "/dev/disk/by-uuid/4FA8-76B6";
+	      # device = "/dev/disk/by-id/boot";
               content = {
                 type = "filesystem";
                 format = "vfat";
