0a5ed695bb6675ef838e983a15e572790476b800
Comamoca <comamoca.dev@gmail.com>
2025-06-22 21:22:42 +0900

    :package: (lspx/default.nix) Add new package lspx

--- pkgs/lspx/default.nix
+++ pkgs/lspx/default.nix
@@ -1,7 +1,7 @@
 # This package help by @kuuote. Thank you!
 { pkgs }:
-        let
-  generated = import ../_sources/generated.nix;
+let
+  generated = import ../../_sources/generated.nix;
   sources = generated {
     inherit (pkgs)
       fetchurl
@@ -11,45 +11,55 @@
       ;
   };
 
-          deps =
-            pkgs.runCommand "lspx-deps"
-              {
-                src = sources.lspx.src;
-                nativeBuildInputs = [
-                  pkgs.deno
-                ];
+  src = sources.lspx.src;
 
-                outputHash = "sha256-cUhCMm0563JNaDQM8NZ1W4oDCy3UjzMp6hP2C0CYncs=";
-                outputHashAlgo = "sha256";
-                outputHashMode = "recursive";
-              }
+  deps =
+    pkgs.runCommand "lspx-deps"
+      {
+        inherit src;
+        nativeBuildInputs = [
+          pkgs.deno
+        ];
 
-              ''
-                unpackPhase
-                cd source
-                HOME="$(mktemp -d)"
-                deno cache --frozen=true --vendor=true main.ts
-                # 一意じゃないと思われるデータの削除
-                for path in vendor/jsr.io/*/*/meta.json; do
-                  echo '{"versions":{}}' > $path
-                done
+        outputHash = "sha256-cUhCMm0563JNaDQM8NZ1W4oDCy3UjzMp6hP2C0CYncs=";
+        outputHashAlgo = "sha256";
+        outputHashMode = "recursive";
+      }
 
-                rm -f node_modules/.deno/.deno.lock.poll
-                mkdir -p $out
-                cp -a node_modules $out/
-                cp -a vendor $out/
-              '';
+      ''
+        unpackPhase
+        cd source
+        HOME="$(mktemp -d)"
+        deno cache --frozen=true --vendor=true main.ts
+        # 一意じゃないと思われるデータの削除
+        for path in vendor/jsr.io/*/*/meta.json; do
+          echo '{"versions":{}}' > $path
+        done
 
-          vendored = pkgs.symlinkJoin {
-            name = "lspx-vendored";
-            paths = [
-              src
-              deps
-            ];
-          };
+        rm -f node_modules/.deno/.deno.lock.poll
+        mkdir -p $out
+        cp -a node_modules $out/
+        cp -a vendor $out/
+      '';
 
-	in {
-          lspx = pkgs.writeShellScript "lspx" ''
-            deno run --allow-env --allow-run --vendor=true --cached-only --node-modules-dir=manual "${vendored}/main.ts" "$@"
-          '';
-}
+  vendored = pkgs.symlinkJoin {
+    name = "lspx-vendored";
+    paths = [
+      src
+      deps
+    ];
+  };
+
+  rawScript = pkgs.writeShellScriptBin "lspx" ''
+    deno run --allow-env --allow-run --vendor=true --cached-only --node-modules-dir=manual "${vendored}/main.ts" "$@"
+  '';
+
+in
+pkgs.runCommand "lspx"
+  {
+    buildInputs = [ pkgs.makeWrapper ];
+  }
+  ''
+    mkdir -p $out/bin
+    makeWrapper ${rawScript}/bin/lspx "$out/bin/lspx"
+  ''
